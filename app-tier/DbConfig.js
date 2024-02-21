5b3f71c2a6d0105752f5d876fcbc44587a7b01e6
wareaws <129090374+wareaws@users.noreply.github.com>
2024-02-21 09:14:33 -0500

    Add files via upload

---
+++ app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'database-1-instance-1.ctjqojps0rjn.us-gov-west-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'password',
+    DB_DATABASE : 'webappdb'
+});
